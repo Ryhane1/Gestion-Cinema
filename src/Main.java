1779ebe4e0cfb59f53c9e98a307be6b664a36739
outamghartAchraf <outamghart2000@gmail.com>
2025-11-28 10:38:19 +0100

    add gestion of spectatur and menu console

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,232 @@
+import models.Film;
+import models.Seance;
+import models.Spectateur;
+import models.Ticket;
+
+import java.sql.Connection;
+
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
-        TicketDAO tdao = new TicketDAO();
+        FilmDAO filmDAO = new FilmDAO();
+        SpectateurDAO spectateurDAO = new SpectateurDAO();
+        SeanceDAO seanceDAO = new SeanceDAO();
+        TicketDAO ticketDAO = new TicketDAO();
 
-        System.out.println("\n===== MENU TICKET =====");
-        System.out.println("1. Ajouter un ticket");
-        System.out.println("2. Modifier un ticket");
-        System.out.println("3. Supprimer un ticket");
-        System.out.println("4. Afficher tous les tickets");
-        System.out.print("Choix : ");
-        int choix = input.nextInt(); input.nextLine();
+        int choice;
 
-        switch(choix) {
+        do {
+            System.out.println("============== CINEMA MENU =================");
+            System.out.println("1-   Add Film");
+            System.out.println("2-   Add Seance");
+            System.out.println("3-   Add Spectateur");
+            System.out.println("4-   Add Ticket");
+            System.out.println("5-   Show All Films");
+            System.out.println("6-   Show All Séances");
+            System.out.println("7-   Show All Spectateurs");
+            System.out.println("8-   Show All Tickets");
+            System.out.println("9-   Delete Spectateur");
+            System.out.println("10-  update Spectateur");
+            System.out.println("11-  Delete Ticket");
+            System.out.println("12-  update Ticket");
+            System.out.println("0-   Exit");
+            System.out.println("============================================");
+            System.out.print("Enter choice : ");
 
-            case 1:
-                System.out.print("ID Ticket : ");
-                int tid = input.nextInt(); input.nextLine();
+            choice = input.nextInt();
+            input.nextLine();
 
-                System.out.print("ID Seance : ");
-                int sid = input.nextInt(); input.nextLine();
+            switch (choice) {
 
-                System.out.print("ID Spectateur : ");
-                int spid = input.nextInt(); input.nextLine();
+                case 1:
+                    System.out.println("=== Add Film ===");
+                    System.out.print("ID: ");
+                    int idF = input.nextInt(); input.nextLine();
+                    System.out.print("Titre: ");
+                    String titre = input.nextLine();
+                    System.out.print("Durée: ");
+                    int duree = input.nextInt(); input.nextLine();
+                    System.out.print("Catégorie: ");
+                    String cat = input.nextLine();
 
-                System.out.print("Prix du ticket : ");
-                double prix = input.nextDouble(); input.nextLine();
+                    Film film = new Film(idF, titre, duree, cat);
+                    filmDAO.ajouterFilm(film);
+                    break;
+                case 2:
+                    System.out.println("=== Add Séance ===");
+                    System.out.print("ID: ");
+                    int idS = input.nextInt(); input.nextLine();
+                    System.out.print("Film ID: ");
 
-                tdao.addTicket(new Ticket( tid ,sid, spid, prix));
-                System.out.println("✔ Ticket ajouté !");
-                break;
+                    int filmId = input.nextInt(); input.nextLine();
+                    Film filmobj = null;
 
-            case 2:
-                System.out.print("ID Ticket à modifier : ");
-                int tidm = input.nextInt(); input.nextLine();
+                    for (Film f : filmDAO.getAllFilms()) {
+                        if (f.getId() == filmId) {
+                            filmobj = f;
+                            break;
+                        }
+                    }
 
-                System.out.print("Nouvelle ID Seance : ");
-                int nsid = input.nextInt(); input.nextLine();
 
-                System.out.print("Nouvelle ID Spectateur : ");
-                int nspid = input.nextInt(); input.nextLine();
+                    if (filmobj == null) {
+                        System.out.println("Film not found.");
+                        break;
+                    }
+                    System.out.print("Horaire: ");
+                    String horaire = input.nextLine();
+                    System.out.print("Salle: ");
+                    String salle = input.nextLine();
+                    System.out.print("Capacité max: ");
+                    int cap = input.nextInt(); input.nextLine();
 
-                System.out.print("Nouveau prix : ");
-                double nprix = input.nextDouble(); input.nextLine();
+                    Seance seance = new Seance(idS, filmobj, horaire, salle, cap);
+                    seanceDAO.ajouterSeance(seance);
+                    break;
 
-                tdao.updateTicket(new Ticket(tidm, nsid, nspid, nprix));
-                System.out.println("✔ Ticket modifié !");
-                break;
+                case 3:
+                    System.out.println("=== Add Spectateur ===");
+                    System.out.print("ID: ");
+                    int idSp = input.nextInt(); input.nextLine();
+                    System.out.print("Nom: ");
+                    String nom = input.nextLine();
+                    System.out.print("Email: ");
+                    String email = input.nextLine();
 
-            case 3:
-                System.out.print("ID Ticket à supprimer : ");
-                int tids = input.nextInt(); input.nextLine();
-                tdao.deleteTicket(tids);
-                System.out.println("✔ Ticket supprimé !");
-                break;
+                    Spectateur spect = new Spectateur(idSp, nom, email);
+                    spectateurDAO.ajouterSpectateur(spect);
+                    break;
 
-            case 4:
-                System.out.println("\n===== LISTE DES TICKETS =====");
-                for (Ticket t : tdao.getAllTickets()) {
-                    System.out.println(t);
-                }
-                break;
+                case 4:
+                    System.out.println("=== Add Ticket ===");
+                    System.out.print("Numéro ticket: ");
+                    int numT = input.nextInt();
+                    System.out.print("Séance ID: ");
+                    int idSe = input.nextInt();
+                    Seance sec = null;
 
-            default:
-                System.out.println("❌ Choix invalide !");
-        }
+                    for (Seance s : seanceDAO.getAllSeances()) {
+                        if (s.getId() == idSe) {
+                            sec = s;
+                            break;
+                        }
+                    }
+
+
+                    if (sec == null) {
+                        System.out.println("Seance not found.");
+                        break;
+                    }
+                    System.out.print("Spectateur ID: ");
+                    int idSpect = input.nextInt();
+
+                    Spectateur specte = null;
+
+                    for (Spectateur spts : spectateurDAO.getAllSpectateurs()) {
+                        if (spts.getId() == idSpect) {
+                            specte = spts;
+                            break;
+                        }
+                    }
+
+
+                    if (specte == null) {
+                        System.out.println("Spectateur not found.");
+                        break;
+                    }
+
+                    System.out.print("Prix: ");
+                    double prix = input.nextDouble();
+
+                     Ticket ticket = new Ticket(numT, sec, specte, prix);
+
+                    ticketDAO.ajouterTicket(ticket);
+                    break;
+
+                case 5:
+                    System.out.println("=== FILMS ===");
+                    for (Film f : filmDAO.getAllFilms()) {
+                        System.out.println(f);
+                    }
+                    break;
+
+                case 6:
+                    System.out.println("=== Séances ===");
+                    for (Seance sea : seanceDAO.getAllSeances()) {
+                        System.out.println(sea);
+                    }
+                    break;
+
+                case 7:
+                    System.out.println("=== Spectateurs ===");
+
+                    for (Spectateur se : spectateurDAO.getAllSpectateurs()) {
+                        System.out.println(se);
+                    }
+                    break;
+
+                case 8:
+                    System.out.println("=== Tickets ===");
+                    for (Ticket t : ticketDAO.getAllTickets()) {
+                        System.out.println(t);
+                    }
+                    break;
+                case 9:
+                    System.out.println("=== Delete Spectateur ===");
+                    System.out.print("ID Spectateur supprimer : ");
+                    int id = input.nextInt();
+                    input.nextLine();
+
+                    boolean deleted = spectateurDAO.deleteSpectateur(id);
+
+                    if (deleted) {
+                        System.out.println("Spectateur supprime succes ");
+                    } else {
+                        System.out.println("Spectateur introuvable ");
+                    }
+                    break;
+                case 10:
+                    System.out.println("=== Update Spectateur ===");
+                    System.out.print("ID du spectateur : ");
+                    int idN = input.nextInt();
+                    input.nextLine();
+
+                    System.out.print("Nouveau nom : ");
+                    String nomNew = input.nextLine();
+
+                    System.out.print("Nouvel email : ");
+                    String emailNew = input.nextLine();
+
+
+                    Spectateur s = new Spectateur(idN, nomNew, emailNew);
+
+                    boolean updated = spectateurDAO.updateSpectateur(s);
+
+                    if (updated) {
+                        System.out.println("Spectateur Update succes");
+                    } else {
+                        System.out.println("Aucun spectateur ");
+                    }
+
+                    break;
+
+
+                case 0:
+                    System.out.println("Exit");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+
+            System.out.println("\n");
+
+        } while (choice != 0);
+
+        input.close();
     }
 }
