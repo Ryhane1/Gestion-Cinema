52cd98218e05c3c1c7f71b1126a16fac6c0f67bd
Ryhane1 <ryhane.harchi@gmail.com>
2025-11-28 10:21:04 +0100

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -4,22 +4,10 @@ import java.sql.SQLException;
 
 public class Database {
 
-    // URL de connexion MySQL (à adapter si nécessaire)
     private static final String URL = "jdbc:mysql://localhost:3307/cinema";
-    private static final String USER = "root";        // ton nom d'utilisateur MySQL
-    private static final String PASSWORD = "";        // ton mot de passe MySQL
+    private static final String USER = "root";        
+    private static final String PASSWORD = "";        
 
-    // Charger le driver (facultatif avec les versions récentes de JDBC)
-    static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Erreur : Driver JDBC introuvable !");
-            e.printStackTrace();
-        }
-    }
-
-    // Méthode pour récupérer une connexion
     public static Connection getConnect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
