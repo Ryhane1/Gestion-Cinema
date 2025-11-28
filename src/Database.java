6e47b420c9da66f587738287961368bdd202d2c5
Ryhane1 <ryhane.harchi@gmail.com>
2025-11-28 10:20:07 +0100

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -7,7 +7,7 @@ public class Database {
     // URL de connexion MySQL (à adapter si nécessaire)
     private static final String URL = "jdbc:mysql://localhost:3307/cinema";
     private static final String USER = "root";        // ton nom d'utilisateur MySQL
-    private static final String PASSWORD = "uxui2025";        // ton mot de passe MySQL
+    private static final String PASSWORD = "";        // ton mot de passe MySQL
 
     // Charger le driver (facultatif avec les versions récentes de JDBC)
     static {
